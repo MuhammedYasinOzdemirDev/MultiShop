63fbb25d2629fd840ceed25f4923ce6c696af645
yasin <cavci48.yz@gmail.com>
2024-07-11 22:13:44 +0300

    feat(logging): configure Serilog and built-in logging in Program.cs
    Added configuration for Serilog and ASP.NET Core built-in logging in Program.cs. The changes include:
    
    - Installed Serilog and related packages.
    - Configured Serilog to read settings from appsettings.json, log to console, and log to file with daily rolling.
    - Set up built-in logging providers for console and debug output.
    - Added comprehensive logging for application startup, including error handling and graceful shutdown.
    
    These changes improve the logging capabilities of the application, providing detailed and structured logs for better observability and troubleshooting.

--- MultiShop.Discount/Program.cs
+++ MultiShop.Discount/Program.cs
@@ -1,8 +1,27 @@
+using MultiShop.Discount.Services;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+
+builder.Host.UseSerilog();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddScoped<IDiscountService, DiscountService>();
+
 
 var app = builder.Build();
 
@@ -12,12 +31,23 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
 
 
 
 app.MapControllers();
 
-app.Run();
 
+try
+{
+    Log.Information("Starting up the application");
+    app.Run();
+}
+catch (Exception ex){
+Log.Fatal(ex, "Application start-up failed");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
