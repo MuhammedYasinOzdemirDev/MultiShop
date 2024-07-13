ff3b87cc9782cf3f1e706937259ebe8cc5b4c05e
yasin <cavci48.yz@gmail.com>
2024-07-13 23:33:54 +0300

    feat(CQRS): Add MediatR handlers and commands for Address entity operations
    - **Commands:**
      - **AddressCommands**
        - CreateAddressCommand.cs: Defines the properties and constructor for creating a new Address.
          - Properties: UserId, District, City, Detail - Details required to create a new Address.
        - RemoveAddressCommand.cs: Defines the properties and constructor for removing an Address.
          - Property: int Id - The ID of the Address to be removed.
        - UpdateAddressCommand.cs: Defines the properties and constructor for updating an Address.
          - Properties: AddressId, UserId, District, City, Detail - Details required to update the Address.
    
    - **Queries:**
      - **AddressQueries**
        - GetAddressByIdQuery.cs: Defines the properties and constructor for querying an Address by ID.
          - Property: int Id - The ID of the Address to retrieve.

--- MultiShop.Order.Application/Features/CQRS/Commands/AddressCommands/CreateAddressCommand.cs
+++ MultiShop.Order.Application/Features/CQRS/Commands/AddressCommands/CreateAddressCommand.cs
@@ -1,6 +1,8 @@
+using MediatR;
+
 namespace MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
 
-public class CreateAddressCommand
+public class CreateAddressCommand:IRequest<int>
 {
     public int UserId { get; set; }
     public string District { get; set; }
--- MultiShop.Order.Application/Features/CQRS/Queries/AddressQueries/GetAddressByIdQuery.cs
+++ MultiShop.Order.Application/Features/CQRS/Queries/AddressQueries/GetAddressByIdQuery.cs
@@ -1,6 +1,9 @@
+using MediatR;
+using MultiShop.Order.Application.Features.CQRS.Results.AddressResults;
+
 namespace MultiShop.Order.Application.Features.CQRS.Queries.AddressQueries;
 
-public class GetAddressByIdQuery
+public class GetAddressByIdQuery: IRequest<GetAddressByIdQueryResult>
 {
     public int Id { get; set; }
 
